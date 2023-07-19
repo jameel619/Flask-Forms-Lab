f5e0f1786b08f99797b335e62ca183dc6b7b8a83
Jameel Saeb <jameelsaeb05@gmail.com>
2023-07-19 07:35:07 +0300

    Done

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -7,16 +7,33 @@ app = Flask(  # Create a flask app
 	static_folder='static'  # Name of directory for static files
 )
 
-
-username = "llo2ay"
-password = "123"
+users = {"adam":"adam123", "loai":"loai123", "jameel":"jameel123"}
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=["GET", "POST"])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == "POST":
+		name_f = request.form["username"].lower()
+		password_f = request.form["password"]
+		try:
+			if users[name_f] == password_f:
+				return redirect(url_for('home'))
+		except:
+			pass
+	return render_template('login.html')
+
+@app.route('/home')
+def home():
+	return render_template('home.html', friends = facebook_friends)
+
+
+@app.route('/friend_exists/<string:name>')
+def friend_exists(name):
+	flag = False
+	if name in facebook_friends:
+		flag = True
+	return render_template('friend_exists.html', flag = flag)
 
 
 
